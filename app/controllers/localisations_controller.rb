ab96e4e27dea9ce2a6a1fabeaeb08401539b454d
root <karine.egler@gmail.com>
2022-11-29 13:54:59 +0100

    localisations

--- app/controllers/localisations_controller.rb
+++ app/controllers/localisations_controller.rb
@@ -1,2 +1,28 @@
 class LocalisationsController < ApplicationController
+
+before_action :set_localisation, only: [:show, :destroy]
+
+def index
+  @localisations = Localisation.all
 end
+
+
+  def create
+   @localisation = Localisation.new(localisation_params)
+      if @localisation.save
+        redirect_to localisation_path(@localisation)
+      else
+        render root_path, status: :unprocessable_entity
+      end
+  end
+
+  private
+
+  def set_localisation
+     @localisation = Localisation.find(params[:id])
+  end
+
+   def localisation_params
+      params.require(:localisation).permit(:coordinates, :user_id)
+   end
+ end
