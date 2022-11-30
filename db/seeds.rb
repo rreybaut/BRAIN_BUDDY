c8d64faf21a3c9b51f8cd580a7bd24bd6d0d0add
raphael reybaut <rreybaut@gmail.com>
2022-11-30 11:26:48 +0100

    reaname title en name ds vocals et on a ajouter destroy chaque tables dans seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Listelement.destroy_all
+List.destroy_all
+Vocal.destroy_all
+Nft.destroy_all
+Localisation.destroy_all
 User.destroy_all
 
-
-User.create!(email: "karine.egler@gmail.com", password:"123456", prenom: "Karine", nom: "Egler", emergency_number: "0641317239")
-User.create!(email: "valerianpallas@gmail.com", password:"123456", prenom: "Valeria", nom: "Pallas",  emergency_number: "0754536825")
-User.create!(email: "rreybaut@gmail.com", password:"123456", prenom: "Raphael", nom: "Reybaut",  emergency_number: "0621888582" )
+User.create!(email: "karine.egler@gmail.com", password: "123456", prenom: "Karine", nom: "Egler", emergency_number: "0641317239")
+User.create!(email: "valerianpallas@gmail.com", password: "123456", prenom: "Valeria", nom: "Pallas",  emergency_number: "0754536825")
+User.create!(email: "rreybaut@gmail.com", password: "123456", prenom: "Raphael", nom: "Reybaut", emergency_number: "0621888582" )
