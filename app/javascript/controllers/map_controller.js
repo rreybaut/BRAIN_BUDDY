5be9ed0874a6f2dac44b007a434e010edaa45e83
root <karine.egler@gmail.com>
2022-11-30 17:58:45 +0100

    mapping

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -15,13 +15,24 @@ export default class extends Controller {
       center: [7.26, 43.72],
       zoom: 10
     })
+
     this.#addMarkersToMap()
+    this.#fitMapToMarkers()
   }
+
+
   #addMarkersToMap() {
+
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
-    })
+      })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
