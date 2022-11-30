5c687c62ecc9323e31d6df1f72f283e7aff25282
valeria pallas <valerianpallas@gmail.com>
2022-11-30 13:23:01 +0100

    controller lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,48 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def new
+    @list = List.new
+
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+
+    if @list.save
+      redirect_to lists_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :date, :user_id)
+  end
 end
