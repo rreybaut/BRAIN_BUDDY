ecebf8be35f392e711de3ceeb9fdb9f76c14d92b
raphael reybaut <rreybaut@gmail.com>
2022-12-01 14:53:34 +0100

    creation .ENV Cloudinary + gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -51,14 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_100130) do
     t.index ["user_id"], name: "index_nfts_on_user_id"
   end
 
-  create_table "urgenceappels", force: :cascade do |t|
-    t.integer "telephone"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_urgenceappels_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -87,6 +79,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_100130) do
   add_foreign_key "lists", "users"
   add_foreign_key "localisations", "users"
   add_foreign_key "nfts", "users"
-  add_foreign_key "urgenceappels", "users"
   add_foreign_key "vocals", "users"
 end
