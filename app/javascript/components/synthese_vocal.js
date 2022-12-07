5133cb8a5f0edc81960666833ddc532c380f613f
raphael reybaut <rreybaut@gmail.com>
2022-12-07 18:08:47 +0100

    comm vocale navigation + enregistrer

--- app/javascript/components/synthese_vocal.js
+++ app/javascript/components/synthese_vocal.js
@@ -1,18 +1,20 @@
 export const syntheseVocal = () => {
-  function speakNow(text) {
-    const message = new SpeechSynthesisUtterance(text);
-    message.lang = "fr-FR";
-
-    const voices = speechSynthesis.getVoices().filter(voice => voice.lang === "en-US");
-    message.voice = voices[0];
-
-    speechSynthesis.speak(message);
-  }
-
-  const title = document.querySelector('.obj-postit:last-child h5').innerHTML;
-  const taskList = document.querySelectorAll('.obj-postit:last-child li a');
   const button = document.querySelector("#speak-now");
-  button.addEventListener("click", () => {
+  if (button) {
+
+    function speakNow(text) {
+      const message = new SpeechSynthesisUtterance(text);
+      message.lang = "fr-FR";
+
+      const voices = speechSynthesis.getVoices().filter(voice => voice.lang === "fr-FR");
+      message.voice = voices[0];
+
+      speechSynthesis.speak(message);
+    }
+
+    const title = document.querySelector('.obj-postit:last-child h5').innerHTML;
+    const taskList = document.querySelectorAll('.obj-postit:last-child li a');
+    button.addEventListener("click", () => {
     speakNow( `Votre liste ${title} contient` );
 
     taskList.forEach((element)=>{
@@ -20,4 +22,5 @@ export const syntheseVocal = () => {
       console.log(element.innerHTML)
     })
   });
+}
 }
