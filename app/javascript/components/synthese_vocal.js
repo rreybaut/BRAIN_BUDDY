7ca144e131175089b87c0adaac1bd70b459d6cac
raphael reybaut <rreybaut@gmail.com>
2022-12-07 11:43:49 +0100

    test param js synhese voc (idem soundrecord eet comm vocale

--- app/javascript/components/synthese_vocal.js
+++ app/javascript/components/synthese_vocal.js
@@ -1,17 +1,21 @@
 
 export const syntheseVocal = () => {
-  function speakNow(text) {
-    const message = new SpeechSynthesisUtterance(text);
-    message.lang = "fr-FR";
+    if (speakNow) {
+      console.log('speak');
 
-//     const voices = speechSynthesis.getVoices().filter(voice => voice.lang === "en-US");
-//     message.voice = voices[0];
+    function speakNow(text) {
+      const message = new SpeechSynthesisUtterance(text);
+      message.lang = "fr-FR";
 
-    speechSynthesis.speak(message);
+  //  const voices = speechSynthesis.getVoices().filter(voice => voice.lang === "en-US");
+  //   message.voice = voices[0];
+
+      speechSynthesis.speak(message);
+    }
+
+    const button = document.querySelector("#speak-now");
+    button.addEventListener("click", () => {
+      speakNow(`Hello Diana sa farte`)
+    });
   }
-
-  const button = document.querySelector("#speak-now");
-  button.addEventListener("click", () => {
-    speakNow(`Hello Diana sa farte`)
-  });
 }
