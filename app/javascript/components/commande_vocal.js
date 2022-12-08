54e9a48128b69d563ac751701d2448b137f0f924
raphael reybaut <rreybaut@gmail.com>
2022-12-08 17:02:53 +0100

    buddy est revenu partial comm vocale et full screen responsive

--- app/javascript/components/commande_vocal.js
+++ app/javascript/components/commande_vocal.js
@@ -10,11 +10,11 @@ export const commandeVocal = () => {
 
     function testSpeech() {
       startTranscript.disabled = true;
-      startTranscript.textContent = 'Test en cours';
+      startTranscript.textContent = 'Audio...';
 
       // To ensure case consistency while checking with the returned output text
 
-      diagnosticPara.textContent = '...diagnostic messages';
+      diagnosticPara.textContent = '...votre message';
 
       var recognition = new SpeechRecognition();
       var speechRecognitionList = new SpeechGrammarList();
@@ -36,7 +36,7 @@ export const commandeVocal = () => {
         // The second [0] returns the SpeechRecognitionAlternative at position 0.
         // We then return the transcript property of the SpeechRecognitionAlternative object
         var speechResult = event.results[0][0].transcript.toLowerCase();
-        diagnosticPara.textContent = 'Speech received: ' + speechResult + '.';
+        diagnosticPara.textContent = 'Message reçu: ' + speechResult + '.';
 
         console.log('Confidence: ' + event.results[0][0].confidence);
 
@@ -63,53 +63,14 @@ export const commandeVocal = () => {
       recognition.onspeechend = function() {
         recognition.stop();
         startTranscript.disabled = false;
-        startTranscript.textContent = 'Nouveau test';
+        startTranscript.textContent = 'Parlez';
       }
 
       recognition.onerror = function(event) {
         startTranscript.disabled = false;
-        startTranscript.textContent = 'Nouveau test';
+        startTranscript.textContent = 'Recommencez';
         diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
       }
-
-      // recognition.onaudiostart = function(event) {
-      //     //Fired when the user agent has started to capture audio.
-      //     console.log('SpeechRecognition.onaudiostart');
-      // }
-
-      // recognition.onaudioend = function(event) {
-      //     //Fired when the user agent has finished capturing audio.
-      //     console.log('SpeechRecognition.onaudioend');
-      // }
-
-      // recognition.onend = function(event) {
-      //     //Fired when the speech recognition service has disconnected.
-      //     console.log('SpeechRecognition.onend');
-      // }
-
-      // recognition.onnomatch = function(event) {
-      //     //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-      //     console.log('SpeechRecognition.onnomatch');
-      // }
-
-      // recognition.onsoundstart = function(event) {
-      //     //Fired when any sound — recognisable speech or not — has been detected.
-      //     console.log('SpeechRecognition.onsoundstart');
-      // }
-
-      // recognition.onsoundend = function(event) {
-      //     //Fired when any sound — recognisable speech or not — has stopped being detected.
-      //     console.log('SpeechRecognition.onsoundend');
-      // }
-
-      // recognition.onspeechstart = function (event) {
-      //     //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-      //     console.log('SpeechRecognition.onspeechstart');
-      // }
-      // recognition.onstart = function(event) {
-      //     //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
-      //     console.log('SpeechRecognition.onstart');
-      // }
     }
 
     startTranscript.addEventListener('click', testSpeech);
